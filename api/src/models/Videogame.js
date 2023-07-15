572718e652abce840cb293a6c4c86f908c4fbb68
Bladimir Parra - IP Proyectos y Soluciones <bladyparra@gmail.com>
2023-07-15 18:50:19 -0500

    Creación de la DB y configuración del server

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,38 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValues: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        notEmpty: true,
+      }
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    platforms: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    image: {
+      type: DataTypes.STRING,
+      defaultValues: 'Unknow',
+      allowNull: true,
+    },
+    released: {
+      type: DataTypes.DATEONLY,
+      allowNull: true,
+    },
+    rating: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
     },
   });
 };
