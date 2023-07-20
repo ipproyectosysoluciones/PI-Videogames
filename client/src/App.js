256ba81af9699bc5f76d83869638aa5af82cd969
Bladimir Parra - IP Proyectos y Soluciones <bladyparra@gmail.com>
2023-07-19 20:31:24 -0500

    Instalación de dependencias del Client, configuración inicial del SPA

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import React from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Landing from './views/Landing/Landing';
+import Home from './views/Home/Home';
+
+import axios from "axios";
+
+axios.defaults.baseURL = 'http://localhost:3001';
 
 function App() {
+  const location = useLocation();
+
+
   return (
     <div className="App">
-      <h1>Henry Videogames</h1>
+      { location.pathname !== '/' }
+      <Routes>
+        <Route path='/' element={ <Landing/> }/>
+        <Route path='/home' element={ <Home/> }/>
+      </Routes>
     </div>
   );
 }
