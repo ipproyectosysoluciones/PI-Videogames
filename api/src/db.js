b01abf272e863ee33045cc4958048a4cd361e04e
Bladimir Parra - IP Proyectos y Soluciones <bladyparra@gmail.com>
2023-08-01 20:01:24 -0500

    Update connection deploy

--- api/src/db.js
+++ api/src/db.js
@@ -3,14 +3,14 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_CONNECTION
+  DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DATABASE
 } = process.env;
 
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/videogames`, {
 //   logging: false, // set to console.log to see the raw SQL queries
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 // });
-const sequelize = new Sequelize(`${DB_CONNECTION}`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DATABASE}`, {
   logging: false, 
   native: false, 
 });
